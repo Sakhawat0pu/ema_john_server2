47b443c92eaaec484fb3cd26217caa1869fa4656
Sakhawat0pu <s19hossain98@gmail.com>
2022-06-21 00:14:15 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
+const app = express();
+require("dotenv").config();
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.hsqji79.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("online_shop");
+    const productCollection = database.collection("products");
+    const orderCollection = database.collection("orders");
+
+    //Get Products API
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
+      const count = await cursor.count();
+      const page = req.query.page;
+      const size = parseInt(req.query.size);
+      let products;
+      if (page) {
+        products = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+      } else {
+        products = await cursor.toArray();
+      }
+      res.send({
+        count,
+        products,
+      });
+    });
+
+    app.post("/products/keys", async (req, res) => {
+      const keys = req.body;
+      const query = { key: { $in: keys } };
+      const products = await productCollection.find(query).toArray();
+      res.json(products);
+    });
+
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.json(result);
+    });
+  } finally {
+    // await client.close()
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Testing the server!!!");
+});
+
+app.listen(port, () => {
+  console.log("listening from port", port);
+});
